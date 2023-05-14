72d876621fbd0eb26655a5fd6372799ad870e2a2
bolla99 <46454991+bolla99@users.noreply.github.com>
2023-05-14 21:24:12 +0200

    Added empty implementation: return nullptr

--- source/GPUComputingCL.cpp
+++ source/GPUComputingCL.cpp
@@ -1,3 +1,14 @@
 //
 // Created by bolla on 12/05/2023.
 //
+
+#include <GPUComputing.hpp>
+#include <iostream>
+
+float* GPUComputing::getGravityFromPointMassesAndDiscreteSpace(
+        const float* pointMasses, int massesSize,
+        const float* discreteSpace, int spaceSize) {
+
+    std::cout << "Operation not supported";
+    return nullptr;
+}
