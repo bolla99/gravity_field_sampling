6d71c2a0804a2344cb98864f7313761e914aeef8
bolla99 <46454991+bolla99@users.noreply.github.com>
2023-05-14 21:18:28 +0200

    Header modified: add two parameters for arrays' size

--- include/GPUComputing.hpp
+++ include/GPUComputing.hpp
@@ -9,7 +9,8 @@
 // implementation will differ based on OS and available GPGPU APIs; implementation file is chosen during build phase;
 class GPUComputing {
 public:
-    static float* getGravityFromPointMassesAndDiscreteSpace(const float* pointMasses, const float* DiscreteSpace);
+    // massesSize and discreteSize refer to previous argumnet length in bytes
+    static float* getGravityFromPointMassesAndDiscreteSpace(const float* pointMasses, int massesSize, const float* discreteSpace, int spaceSize);
 };
 
 #endif //GL_TEST_PROJECT_GPUCOMPUTING_HPP
