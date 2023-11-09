56b709133bf125a74770712258e7b1049a43f473
bolla99 <46454991+bolla99@users.noreply.github.com>
2023-11-09 15:31:22 +0100

    timer class added
    timer class: tiny wrapper around sdl timer function and simplified log time for performance mesurements

---
+++ include/timer.hpp
@@ -0,0 +1,28 @@
+//
+// Created by Giovanni Bollati on 09/11/23.
+//
+
+#ifndef GL_TEST_PROJECT_TIMER_HPP
+#define GL_TEST_PROJECT_TIMER_HPP
+
+#include <SDL.h>
+
+class Timer {
+private:
+    int id;
+    Uint64 ms;
+public:
+    Timer() : ms(SDL_GetTicks64()) {
+        static int _id = 0;
+        id = _id++;
+    }
+    int time() { return SDL_GetTicks64() - ms; }
+
+    void log() {
+        std::cout << std::endl << "timer " << id;
+        if(ms > 0) std::cout << " | time elapsed: " << time();
+        std::cout << std::endl;
+    }
+};
+
+#endif //GL_TEST_PROJECT_TIMER_HPP
